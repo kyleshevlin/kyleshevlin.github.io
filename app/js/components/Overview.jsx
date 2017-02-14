519dc0cb818ba84bfb722ff1e20a333ea003c5ef
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-02-14 09:50:32 -0800

    update resume

--- app/js/components/Overview.jsx
+++ app/js/components/Overview.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 
-const Overview = () => {
-  return (
-    <div className='overview'>
-      <h2>Overview</h2>
-      <p>
-        Front end developer with some full stack chops. Strong experience with React, Ember, Rails, and Sass. Also experienced in building/consuming APIs as well as building data visualizations with d3.js.
-      </p>
+const Overview = () => (
+  <div className='overview'>
+    <h2>Overview</h2>
+    <p>
+      Front end developer with some full stack chops. Strong experience with React, Ember, Rails, and Sass. Also experienced in building/consuming APIs and building data visualizations with d3.js.
+    </p>
 
-      <blockquote>
-        I am endlessly curious and always ready for a new challenge.
-      </blockquote>
+    <blockquote>
+      I am endlessly curious and always ready for a new challenge.
+    </blockquote>
 
-      <p>
-        Currently, I am focused on becoming the best JavaScript developer I can be and working hard to build some great tools and products along the way.
-      </p>
-    </div>
-  )
-}
+    <p>
+      Currently, I am focused on becoming the best JavaScript engineer I can be and building some great tools and products along the way.
+    </p>
+  </div>
+)
 
 export default Overview
